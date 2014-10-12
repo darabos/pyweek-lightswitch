c5b4894da280a166a2078168da3dc48f562d27ac
Daniel Darabos <darabos.daniel@gmail.com>
2014-10-12 16:51:23 +0200

    Packaging script.

---
+++ dist.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -xue
+
+VERSION=1.1
+DIST=pyweek-lightswitch-$VERSION
+
+rm -rf $DIST || true
+mkdir $DIST
+cp -r *.ttf run_game.py picture_render.py README.md pictures_vbuf.zip sounds $DIST
+COPYFILE_DISABLE=1 zip -r $DIST.zip $DIST
